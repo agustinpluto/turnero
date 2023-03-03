b96f8e25851f459b9ad21a05451ce0fcc9b28b3f
Agustin PLuto <agustinpluto@gmail.com>
2023-03-03 20:06:58 -0300

    asd

--- conexion.php
+++ conexion.php
@@ -6,18 +6,4 @@ if (!$con) {
     die("Error de conexión: " . mysqli_connect_error());
 }
 
-$sql = "SELECT * FROM usuarios";
-$resultados = mysqli_query($con, $sql);
-
-if (!$resultados) {
-    die("Error en la consulta: " . mysqli_error($con));
-}
-
-if (mysqli_num_rows($resultados) > 0) {
-    while ($fila = mysqli_fetch_assoc($resultados)) {
-        echo $fila["id"] . " " . $fila["nombre"] . " " . $fila["apellido"] . "<br>";
-    }
-} else {
-    echo "No se encontraron resultados en la base de datos.";
-}
 ?>
