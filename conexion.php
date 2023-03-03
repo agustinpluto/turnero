75bdee285a6e8caf3c9843fe07e44ddd5b9aded2
Agustin PLuto <agustinpluto@gmail.com>
2023-03-03 19:38:47 -0300

    first commit

---
+++ conexion.php
@@ -0,0 +1,12 @@
+<?php
+
+      $con = mysqli_connect("localhost", "agustin", "Ireliagod1!", "apirest");
+      
+      $sql = "SELECT * FROM usuarios";
+      $resultados = mysqli_query($con, $sql);
+
+      $fila = mysqli_fetch_assoc($resultados);
+
+      echo $fila[1];
+
+?>
