aea8f07e2a178e2e36b5444fa101e792f7212fe4
Agustin PLuto <agustinpluto@gmail.com>
2023-03-03 19:57:11 -0300

    asd

--- conexion.php
+++ conexion.php
@@ -5,13 +5,12 @@ $con = mysqli_connect("localhost", "agustin", "Ireliagod1!", "apirest");
 $sql = "SELECT * FROM usuarios";
 $resultados = mysqli_query($con, $sql);
 
-$fila = mysqli_fetch_assoc($resultados);
-
 if (!$resultados) {
     die("Error en la consulta: " . mysqli_error($con));
 }
 
 while ($fila = mysqli_fetch_assoc($resultados)) {
-    echo $fila["id"] . " " . $fila["nombre"] . " " . $fila["apellido"];
+    echo $fila["id"] . " " . $fila["nombre"] . " " . $fila["apellido"] . "\n";
 }
+
 ?>
